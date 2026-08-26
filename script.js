37625ae173bd725e6b5821ab7866c684ba4203bc
UnknownxD2 <adhwaithspillai051@gmail.com>
2026-08-27 01:37:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ const memories = [
 
     {
         photo: "photos/photo12.jpeg",
-        text: "BEST ONE SO FARR FOR ME JKNSNDFJHFL"
+        text: "BEST ONE FOR ME SO FARR HFGASJYGJKNSNDFJHFL"
     }
 
 ];
